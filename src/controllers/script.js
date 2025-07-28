e0c26ac5e4de74e84ebf049e4346cd2c66bf83f4
Reinaldo Fernandes <reinaldo.f.menezes@outlook.com>
2025-07-28 14:29:34 -0300

    inicio

---
+++ src/controllers/script.js
@@ -0,0 +1,16 @@
+/** 
+ Adicionar Itens ao Pedido: Escrever a lógica para quando um item for clicado no menu, ele seja adicionado à lista de pedidos e seu preço seja somado ao total.
+ _____________________________________________________________
+Calcular o Total: Atualizar o total da conta sempre que um item for adicionado ou removido do pedido.
+ _____________________________________________________________
+
+Remover Itens do Pedido: Adicionar funcionalidade para o atendente remover itens do pedido, se necessário.
+ _____________________________________________________________
+
+Mostrar e Esconder Notificações: Criar lógica para mostrar notificações de novos pedidos ou alterações.
+ _____________________________________________________________
+
+Persistência (opcional): Usar LocalStorage para salvar o pedido enquanto o cliente navega na página.
+ _____________________________________________________________
+
+Finalizar Pedido: Criar uma função para "finalizar" o pedido, onde você pode fazer o envio do pedido ou simplesmente mostrar a confirmação.**/
